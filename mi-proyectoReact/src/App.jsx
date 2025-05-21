c7d9c658a932ff9496fad837dcf99d9dc68fbd88
albecabrera <albe.cabrera@yahoo.de>
2025-05-21 21:36:16 +0200

    cambio 2

--- mi-proyectoReact/src/App.jsx
+++ mi-proyectoReact/src/App.jsx
@@ -13,45 +13,28 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <Router>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Inicio</Link>
-            </li>
-            <li>
-              <Link to="/Sobre Mi">Sobre Mi</Link>
-            </li>
-            <li>
-              <Link to="/Proyectos">Proyectos</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <Route exact path="/" component={Inicio} />
-      <Route path="/Sobre Mi" component={SobreMi} />
-      <Route path="/Proyectos" component={Proyectos} />
+      
+  
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Inicio</Link>
+              </li>
+              <li>
+                <Link to="/Sobre Mi">Sobre Mi</Link>
+              </li>
+              <li>
+                <Link to="/Proyectos">Proyectos</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Route exact path="/" component={Inicio} />
+        <Route path="/Sobre Mi" component={SobreMi} />
+        <Route path="/Proyectos" component={Proyectos} />
     </Router>
   )
 }
